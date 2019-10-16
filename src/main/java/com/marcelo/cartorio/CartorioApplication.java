4b091d056b8637e2c3ca82cfed2fd18bf0324259
Marcelo Teixeira <celocesar30@hotmail.com>
2019-10-16 13:00:47 -0300

    Projeto criado

---
+++ src/main/java/com/marcelo/cartorio/CartorioApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo.cartorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartorioApplication.class, args);
+	}
+
+}
