a85f273e07e21ef13ab714e0bf3acb0e14429197
Marcelo Teixeira <celocesar30@hotmail.com>
2019-10-16 17:57:22 -0300

    Criação dos métodos na classe service

--- src/main/java/com/marcelo/cartorio/services/CartorioService.java
+++ src/main/java/com/marcelo/cartorio/services/CartorioService.java
@@ -1,5 +1,6 @@
 package com.marcelo.cartorio.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +23,34 @@ public class CartorioService {
 
 	public Cartorio visualizar(Integer id) {
 		Optional<Cartorio> obj = repo.findById(id);
+
 		return obj.orElse(null);
+	}
+
+	// Método para criar um cartório
+
+	public Cartorio criar(Cartorio obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	// Método para editar um cartório
+
+	public Cartorio update(Cartorio obj) {
+		visualizar(obj.getId());
+		return repo.save(obj);
+	}
+
+	// Método para excluir um cartório
+	public void Excluir(Integer id) {
+		visualizar(id);
+
+		repo.deleteById(id);
+	}
+
+	// Método para listar todos os cartórios
+	public List<Cartorio> findAll() {
+		return repo.findAll();
 	}
 
 }
