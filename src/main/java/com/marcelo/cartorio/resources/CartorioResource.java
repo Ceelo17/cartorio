f9b88fd0f39b78c0f445521e0166eb8dac2f7366
Marcelo Teixeira <celocesar30@hotmail.com>
2019-10-16 13:45:53 -0300

    Criação e teste da classe domain

--- src/main/java/com/marcelo/cartorio/resources/CartorioResource.java
+++ src/main/java/com/marcelo/cartorio/resources/CartorioResource.java
@@ -1,16 +1,29 @@
 package com.marcelo.cartorio.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.marcelo.cartorio.domain.Cartorio;
 
 @RestController
 @RequestMapping(value = "/cartorios")
 public class CartorioResource {
 
-	@RequestMapping(method = RequestMethod.GET)	
-	public String listar() {
-		return "REST esta funcionando";
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Cartorio> listar() {
+
+		Cartorio car1 = new Cartorio(null, "Cartório1");
+		Cartorio car2 = new Cartorio(null, "Cartorio2");
+
+		List<Cartorio> lista = new ArrayList<>();
+		lista.add(car1);
+		lista.add(car2);
+
+		return lista;
 	}
 
 }
